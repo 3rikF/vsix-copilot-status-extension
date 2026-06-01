dc61fd0ff54e020987811bd8e252ece0c28a5b72
3rikF <70095154+3rikF@users.noreply.github.com>
2026-06-01 20:31:30 +0200

    refactor: Added the `StatusInfoPopupControl` user-control
    Created the new `StatusInfoPopupControl` `UserControl` and its associated code-behind file.
    The XAML implements a UI for displaying status information, including resources, styles, and data binding to a ViewModel.
    The code-behind file contains the initialization constructor.

---
+++ CoPilotStatusExtension/Views/StatusInfoPopupControl.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+//-----------------------------------------------------------------------------------------------------------------------------------------
+namespace CoPilotStatusExtension.Views;
+
+//-----------------------------------------------------------------------------------------------------------------------------------------
+public partial class StatusInfoPopupControl : UserControl
+{
+	public StatusInfoPopupControl()
+		=> InitializeComponent();
+}
