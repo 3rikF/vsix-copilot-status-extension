f03803a0f15c6b25db9a22b3c647968ddf324a04
3rikF <70095154+3rikF@users.noreply.github.com>
2026-06-04 23:53:46 +0200

    refactor: Replace specific converters with `SimpleTranslationConverter`
    The previous specific converters for subscription and plan have been replaced with a generic `SimpleTranslationConverter`.
    The converter for status has been removed .
    The translation logic is now organized centrally in a static class with a dictionary.

---
+++ CoPilotStatusExtension/Views/Converters/ConverterSimpleTranslation.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+//-----------------------------------------------------------------------------------------------------------------------------------------
+namespace CoPilotStatusExtension.Views.Converters;
+
+//-----------------------------------------------------------------------------------------------------------------------------------------
+file static class SimpleStaticTranslations
+{
+	private readonly static Dictionary<string, string> TRANSLATIONS = new()
+	{
+		//--- account type ---
+		{ "individual",						"Individual"},
+		{"business",						"Organization"},
+		{"enterprise",						"Enterprise"},
+
+		//--- CoPilot subscription status ---
+		{"free_limited_quota",				"Free (limited quota)"},
+		{"trial_subscriber_quota",			"Trial Subscriber"},
+		{"yearly_subscriber_quota",			"Yearly Subscriber"},
+		{"copilot_enterprise_seat_quota",	"Copilot Enterprise"},
+		{"copilot_for_business_seat_quota", "Copilot for Business"},
+
+		//--- quota IDs ---
+		{"chat",							"Chat"},
+		{"completions",						"Completions"},
+		{"premium_interactions",			"AI-Credits"},
+	};
+
+	public static bool TryGetValue(string key, out string? value)
+		=> TRANSLATIONS.TryGetValue(key, out value);
+
+	public static string Translate(string key)
+	{
+		if (TRANSLATIONS.TryGetValue(key, out var value))
+			return value;
+		else
+			return $"Unknown [{key}]";
+	}
+}
+
+//-----------------------------------------------------------------------------------------------------------------------------------------
+public sealed class SimpleTranslationConverter : IValueConverter
+{
+	public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		if (value is string key && !string.IsNullOrWhiteSpace(key))
+			return SimpleStaticTranslations.Translate(key);
+
+		else
+			//return DependencyProperty.UnsetValue;
+			return Binding.DoNothing;
+	}
+
+	public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		=> throw new NotSupportedException();
+}
